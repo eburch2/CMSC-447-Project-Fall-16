ca9c3488f837ce80f86ddaed3d2f101d1ceb4ec7
jersonguansing <jersonguansing@gmail.com>
2016-11-01 22:08:07 -0400

    This resets the password for users who forgot their login credential. Also, the login system will block any attempts after 15 failed logins based on their IP address. The file, resetpass.php will circumvent this by resetting the attempt counter back to zero after a new password has been sent to the user's email

---
+++ src/resetpass.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+require("CommonMethods.php");
+require("DBConnection.php");
+require("UMBCDirectory.php");
+
+$errorMessage = "";
+$successful = false;
+// check the session if there is already a user logged in
+// check if the session is expired (no activity within 30 minutes) and retrieve user info stored in the session
+$userInfo = sessionInfo();
+if(count($userInfo) > 0) {
+	// the user is already logged in, they cannot register an account without signing out
+	// redirect to the main page
+	header("Location: index.php");
+	exit();
+}
+else {
+	$umbcid = "";
+	$username = "";
+	$username_min = 7;
+	$username_max = 50;
+	$captcha = "";
+
+	if(!empty($_POST["umbcid"])) {
+		$umbcid = htmlspecialchars(strtoupper($_POST["umbcid"]));
+		// regular expression to check the umbcid
+		if(!preg_match("/^[A-Z]{2}[0-9]{5}$/", $umbcid)) {
+			$umbcid = "";
+		}
+	}
+	if(!empty($_POST["username"])) {
+		$username = htmlspecialchars($_POST["username"]);
+		// username must bet at between 7 and 50 characters long
+		if(!(strlen($username) >= $username_min && strlen($username) <= $username_max)) {
+			$username = "";
+		}
+		// regular expression to check if the username is alphanumeric
+		if(!preg_match("/^[A-Za-z0-9]+$/", $username)) {
+			$username = "";
+		}
+	}
+	if(!empty($_POST["captcha"])) {
+		$captcha = htmlspecialchars($_POST["captcha"]);
+		// regular expression to check the lastname
+		if($captcha != $_SESSION["umbcbazaar_captcha"]) {
+			$captcha = "";
+		}
+	}
+	if($umbcid != "" && $username != "" && $captcha != "") {
+		$db1 = new DBConnection;
+		// check if username is already taken
+		$row = $db1 -> select("SELECT COUNT(*) FROM users WHERE username = \"" . bin2hex($username) . "\" AND umbcid = \"" . $umbcid . "\" AND status >= 0 LIMIT 1 ");
+		if($row[0]["COUNT(*)"] > 0) {
+			$newPassword = "";
+			$options = array();
+			for($i=48; $i < 58; $i++) {
+				array_push($options, chr($i));
+			}
+			for($i=65; $i < 91; $i++) {
+				array_push($options, chr($i));
+			}
+			for($i=97; $i < 123; $i++) {
+				array_push($options, chr($i));
+			}
+			array_push($options, chr(33));
+			array_push($options, chr(64));
+			array_push($options, chr(35));
+			array_push($options, chr(36));
+			array_push($options, chr(37));
+			
+			for($i=0; $i < 20; $i++) {
+				$newPassword .= $options[rand(0, count($options)) - 1];
+			}
+			if(!preg_match("/.*[!@#\$%].*$/", $newPassword)) {
+				$newPassword .= $options[rand(62, count($options)) - 1];
+			}
+			if(!preg_match("/.*[0-9].*$/", $newPassword)) {
+				$newPassword .= chr(rand(48, 57));
+			}
+			if(!preg_match("/.*[A-Z].*$/", $newPassword)) {
+				$newPassword .= chr(rand(65, 90));
+			}
+			if(!preg_match("/.*[a-z].*$/", $newPassword)) {
+				$newPassword .= chr(rand(97, 122));
+			}
+			//echo $newPassword;
+			$hashPass = hashPassword($newPassword);
+			$db1 -> query("UPDATE users SET password = '" . bin2hex($hashPass) . "' WHERE umbcid = '" . $umbcid . "' AND username = '" . bin2hex($username) . "' ");
+			
+			$emailBody = "Your password has been reset. Your new password is listed below:<br/>\n\n";
+			$emailBody .= $newPassword;
+			$sendEmailURL = $EMAIL_SERVER . "/sendemail.php?umbcid=" . $umbcid . "&subject=" . bin2hex("Password Reset") . "&body=" . bin2hex($emailBody);
+			$emailStatus = curl($sendEmailURL);
+			$errorMessage .= "Password has been reset! Please check your email for further instructions.";
+			$successful = true;
+		}
+		else {
+			$errorMessage .= "Password reset failed! Please make sure you entered the correct UMBC ID and username for an active account.";
+		}
+		$db1 -> close();
+	}
+}
+
+include("top.html");
+?>
+<font color="white"></font>
+<?php
+include("middle.html");
+if($errorMessage != "") {
+	echo "<span class=\"errorMessage\">" . $errorMessage . "</span><br/><br/>";
+	if($successful == true) {
+		?>
+		<a href="login.php"><input class="button2" type="button" value="Log in to your Account"/></a>
+		<?php
+	}
+	else {
+		?>
+		<a href="resetpass.php"><input class="button2" type="button" value="Try resetting Password"/></a>
+		<?php
+	}
+	
+}
+else {
+	?>
+	Please enter your student information.<br/><br/>
+			<form action="resetpass.php"  method="post" >
+				<table border=0 align="center">
+				<tr>
+				<td>UMBC ID</td><td><input class="loginInput" type="text" id="umbcid" name="umbcid" title="Please enter your student ID. The format needs to be 2 characters followed by 5 digits (AAXXXXX)."  value="" placeholder="AAXXXXX   A=char X=digit" maxlength="7" pattern="[A-Za-z]{2}[0-9]{5}" required autofocus /></td>
+				</tr>
+				<tr>
+				<td>Username</td><td><input class="loginInput" type="text" id="username" name="username" title="Please enter your firstname. Only alphanumeric are allowed. Must be at least 7 characters long" value="" placeholder="Username at least 7 characters long" maxlength="50" pattern="^[A-Za-z0-9]{7,50}" required /></td>
+				</tr>
+				<tr>
+				<td>Captcha</td>
+				<td><img src="captcha.php" width="240" height="50" /><br/><input class="loginInput" type="text" id="captcha" name="captcha" width="100" title="Please enter the text from the image above." placeholder="Match the text in the image" value="" required /></td>
+				</tr>
+				<tr>
+				<td colspan="2"><br/><input class="button1" type="submit" value="Reset Password" /></td>
+				</tr>
+				</table>
+			</form>
+			<script>
+				document.querySelector("#umbcid").addEventListener("input", formatUMBCID);
+				document.querySelector("#username").addEventListener("input", formatUsername);
+			</script>
+	<?php
+}
+
+include("bottom.html");
+?>
